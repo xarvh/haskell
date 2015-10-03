a1c13f8ce4d6bcc1774a0dc401346dbb685a0c5a
xarvh <francesco.orsenigo@adslot.com>
2015-10-04 10:51:02 +1100

    use list

--- calc.hs
+++ calc.hs
@@ -2,27 +2,32 @@
 import qualified Data.Char as C
 
 
-operators = "+-*/"
+
+-- let's use a string: in principle, an operator could be more than two chars
+operators :: [([Char], Double -> Double -> Double)]
+operators = [ ("+", (+)), ("-", (-)) ]
+
+
+opEq :: String -> (String, a) -> Bool
+opEq string op = ((fst op) == string)
+
 
 stringToOperator :: String -> Double -> Double -> Double
-stringToOperator "+" = (+)
-stringToOperator "-" = (-)
-stringToOperator "*" = (*)
-stringToOperator "/" = (/)
-stringToOperator s = error ("Syntax error at operator " ++ s)
+stringToOperator string = snd (head (filter (opEq string) operators))
 
 
 isOperator :: Char -> Bool
-isOperator = (`elem` operators)
+isOperator = (`elem` (concatMap fst operators))
 
 
 -- evaluates a simple expression "23 - 5" into a double
 calc :: String -> Double
 calc string =
-  let trimmedString = filter (not . C.isSpace) string
-      (leftOperand, remainder) = break isOperator trimmedString
-      (operator, rightOperand) = break (not . isOperator) remainder
-      op = stringToOperator operator
-      le = read leftOperand :: Double
-      ri = read rightOperand :: Double
+  let
+    trimmedString = filter (not . C.isSpace) string
+    (leftOperand, remainder) = break isOperator trimmedString
+    (operator, rightOperand) = break (not . isOperator) remainder
+    op = stringToOperator operator
+    le = read leftOperand :: Double
+    ri = read rightOperand :: Double
   in op le ri
